c144546d9859ec3d49934f44c7b83d411d0e3a99
Ardinsyah Siregar <94067313+Ardin13@users.noreply.github.com>
2025-03-05 11:56:57 +0700

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,11 @@
+<?php
+include("conection.php");
+
+$id = $_GET["id"];
+$query = mysqli_query($connection,"SELECT * FROM pegawai WHERE id = $id");
+$pegawai = mysqli_fetch_assoc($query);
+
+echo "<pre>";
+print_r($pegawai);
+
+
